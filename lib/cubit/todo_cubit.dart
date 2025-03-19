00c2f5deb6e19f6ee88232903dcfa216c4254259
Sandun Induranga <sandutharu40@gmail.com>
2025-03-19 22:18:31 +0530

    add TodoCubit for managing todo state and update dependencies

--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -1,1 +1,41 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../data/model/todo.dart';
+
+class TodoState {
+  final List<Todo> todos;
+
+  TodoState(this.todos);
+}
+
+class TodoCubit extends Cubit<TodoState> {
+  TodoCubit() : super(TodoState([]));
+
+  void addTodo(String title) {
+    final newTodo = Todo(
+      id: DateTime.now().toString(),
+      title: title,
+    );
+    emit(
+      TodoState(
+        [...state.todos, newTodo],
+      ),
+    );
+  }
+
+  void toggleTodo(String id) {
+    final updatedTodos = state.todos.map((todo) {
+      if (todo.id == id) {
+        return Todo(
+          id: todo.id,
+          title: todo.title,
+          isCompleted: !todo.isCompleted,
+        );
+      }
+      return todo;
+    }).toList();
+    emit(
+      TodoState(updatedTodos),
+    );
+  }
+}
