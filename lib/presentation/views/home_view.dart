f1f933f70e89808a4f10459abdd0d9f280da45a7
Sandun Induranga <sandutharu40@gmail.com>
2025-03-19 22:02:28 +0530

    add initial HomeView widget with basic layout

---
+++ lib/presentation/views/home_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+            ],
+          ),
+      ),
+    );
+  }
+}
+
